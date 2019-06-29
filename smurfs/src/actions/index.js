409511b010b529716bd7cd18a286587011984260
Robert Gant <demontegant@gmail.com>
2019-06-29 12:07:34 -0500

    finally got it functional after a while. now i am about to do the styling on my prject

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,29 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios';
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+export const GETTING_SMURFS = 'ADD_SMURF';
+export const GOT_SMURFS = 'GOTSMURFS';
+export const ADDED_SMURF = 'ADDED_SMURF';
+export const DELETED_SMURF = 'DELETED_SMURF';
+
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: GETTING_SMURFS });
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(res => dispatch({ type: GOT_SMURFS, payload: res.data }))
+    .catch(err => console.log(err));
+};
+
+export const addSmurf = smurf => dispatch => {
+  dispatch({ type: GETTING_SMURFS });
+  axios
+    .post('http://localhost:3333/smurfs', smurf)
+    .then(res => dispatch({ type: ADDED_SMURF, payload: res.data }));
+};
+
+export const deleteSmurf = id => dispatch => {
+  dispatch({ type: GETTING_SMURFS });
+  axios
+    .delete(`http://localhost:3333/smurfs/${id}`)
+    .then(res => dispatch({ type: DELETED_SMURF, payload: res.data }))
+    .catch(err => console.log(err));
+};
